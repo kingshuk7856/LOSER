3e7b0820f757497a95e97306917d40cfb352b777
kingshuk7856 <71704935+kingshuk7856@users.noreply.github.com>
2022-07-19 15:07:48 +0530

    Delete js directory

--- js/sc.js
+++
@@ -1,8 +0,0 @@
-setInterval(() => {
-    dt = new Date()
-    dom1 = document.querySelector("#time");
-    dom2 = document.querySelector("#date");
-    
-    dom1.innerHTML = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-    dom2.innerHTML = dt.toLocaleDateString();
-}, 1000);
