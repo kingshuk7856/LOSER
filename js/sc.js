dd257e3f475be79a43722ab3f31e46c3fa239112
[kingshuk7856] <zarurat7856@gmail.com>
2022-07-19 16:07:11 +0530

    1st-edit

--- js/sc.js
+++ js/sc.js
@@ -2,7 +2,11 @@ setInterval(() => {
     dt = new Date()
     dom1 = document.querySelector("#time");
     dom2 = document.querySelector("#date");
+    var options = { weekday: "long", 
+                    year: "numeric", 
+                    month: "short", 
+                    day: "numeric" };
     
     dom1.innerHTML = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-    dom2.innerHTML = dt.toLocaleDateString();
+    dom2.innerHTML = dt.toLocaleDateString("en-IN",options);
 }, 1000);
